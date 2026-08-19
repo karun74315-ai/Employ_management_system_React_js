a3c0adc296e0f154ca95dc8bef2154544a4e244b
Arun Kumar <karun74315@gmail.com>
2026-08-19 20:16:09 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,28 @@ const   App = ()=> {
 
 const[user, setUser]  = useState(null)
 const authdata = useContext(Authcontext)
-console.log(authdata)
+// console.log(authdata)
+
+useEffect(()=>{
+  if(authdata){
+    const loggedInUser = localStorage.getItem("loggedInUser")
+     if(loggedInUser){
+      setUser(loggedInUser.role)
+     }
+
+  }
+},[authdata])
 
  const handleLogin = (email,password)=>{
       
       if(email==='admin@me.com' && password === "123"){
            setUser('admin')
+           localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}))
+
       }else if(authdata && authdata.employees.find((e) => e.email == email && e.password== password)){
         setUser('employees') 
+        localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'}))
+
       }
       else{
         alert('invalid')
