fcd2504a0642ad7e0ccfa8da20a03b7a2d092adb
Arun Kumar <karun74315@gmail.com>
2026-09-12 20:22:17 +0530

    changes in the file

--- src/Components/Taklist/TaskList.jsx
+++ src/Components/Taklist/TaskList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TaskList = () => {
+const TaskList = ( {data}) => {
   return (
     <div id='taskl' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap  w-full py-5  mt-10'>
       
